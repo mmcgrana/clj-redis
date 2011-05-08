650912bfcd599166d6143bbf256511e8fcbadd62
mpenet <max.penet@gmail.com>
2011-05-08 07:48:16 +0200

    use Jedis 1.5.2 and remove apache pool from dependencies

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
 (defproject clj-redis "0.0.9"
   :dependencies
     [[org.clojure/clojure "1.3.0-alpha4"]
-     [redis.clients/jedis "1.5.0"]
-     [commons-pool/commons-pool "1.5.5"]])
+     [redis.clients/jedis "1.5.2"]])
