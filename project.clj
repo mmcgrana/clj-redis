29969d159b8d7148408da3de5aa1156d75579f9b
David Czarnecki <czarneckid@acm.org>
2011-07-19 18:39:47 -0400

    Added lein-related ignore and added some project information

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject clj-redis "0.0.12"
+  :description "Clojure Redis client library"
+  :url "https://github.com/mmcgrana/clj-redis"
   :dependencies
     [[org.clojure/clojure "1.3.0-alpha4"]
      [redis.clients/jedis "1.5.2"]])
