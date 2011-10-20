ebcf1d6581c0f3a762cefe6195cda96e56c089bd
Phil Hagelberg <technomancy@gmail.com>
2011-10-20 13:29:24 -0700

    Bump to version 0.0.13-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
-(defproject clj-redis "0.0.12"
+(defproject clj-redis "0.0.13-SNAPSHOT"
   :description "Clojure Redis client library"
   :url "https://github.com/mmcgrana/clj-redis"
-  :dependencies
-    [[org.clojure/clojure "1.3.0-alpha4"]
-     [redis.clients/jedis "1.5.2"]])
+  :dependencies [[redis.clients/jedis "1.5.2"]]
+  :dev-deps [[org.clojure/clojure "1.3.0"]])
