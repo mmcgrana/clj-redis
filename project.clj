3e783e09c6eeb16a03c42bcaca16419daef1b078
Phil Hagelberg <technomancy@gmail.com>
2011-10-20 15:26:39 -0700

    Added lein-multi to project.clj.

--- project.clj
+++ project.clj
@@ -1,5 +1,10 @@
 (defproject clj-redis "0.0.13-SNAPSHOT"
   :description "Clojure Redis client library"
   :url "https://github.com/mmcgrana/clj-redis"
-  :dependencies [[redis.clients/jedis "1.5.2"]]
-  :dev-deps [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure "[1.2.0,1.3.0]"]
+                 [redis.clients/jedis "1.5.2"]]
+  :multi-deps {"1.3" [[org.clojure/clojure "1.3.0"]
+                      [redis.clients/jedis "1.5.2"]]
+               "1.2" [[org.clojure/clojure "1.2.1"]
+                      [redis.clients/jedis "1.5.2"]]}
+  :dev-deps [[lein-multi "1.0.0"]])
